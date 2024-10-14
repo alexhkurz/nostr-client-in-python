5c3b12ee67515229a67c2fbc9e57f70da2c89893
Alexander Kurz <alexhkurz@gmail.com>
2024-10-13 20:28:38 -0700

    clean up

--- setup.sh
+++ setup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Deactivate any number of nested virtual environments
-while [ -n "$VIRTUAL_ENV" ]; do
-    deactivate
-done
-
-# Remove existing venv if it exists
-if [ -d "venv" ]; then
-    rm -rf venv
-fi
-
 # Create and activate a new virtual environment
 python3 -m venv venv
 source venv/bin/activate
